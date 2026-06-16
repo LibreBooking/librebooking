4a86ea41674e604e9fc205dd734e88d9474ec14f
barakiva <barakiva1@gmail.com>
2026-06-16 14:53:06 +0300

    test(e2e): an example of how a refactor should be made on tpl to make playwright locator more robust

---
+++ tests/e2e/README.md
@@ -0,0 +1,9 @@
+# Integration testing (E2E)
+
+Playwright is the modern way to E2E test web applications. It uses a headless browser to simulate user actions, allowing us to automate hours of painstaking QA in minutes.
+
+
+
+## Guidelines
+
+The most robust way to allow the playwright locator to find elements in the web page is by adding a data-testid to each element we'd like to test.
--- tests/e2e/auth.setup.ts
+++ tests/e2e/auth.setup.ts
@@ -6,9 +6,9 @@ const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 setup('authenticate', async ({ page }) => {
   // Perform authentication steps. Replace these actions with your own.
   await page.goto('http://localhost:8080/');
-  await page.getByPlaceholder('Username or Email').fill('admin');
-  await page.getByPlaceholder('Password').fill('password');
-  await page.getByRole('button', { name: /Log In/i }).click();
+  await page.getByTestId('login-username').fill('admin');
+  await page.getByTestId('login-password').fill('password');
+  await page.getByTestId('login-submit').click();
   // Wait until the page receives the cookies.
   //
   // Sometimes login flow sets cookies in the process of several redirects.
